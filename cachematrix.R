d17dae9ca7792c6188f002838bb3a4795f244251
guyvorster <contact@guyvorster.com>
2014-10-21 16:38:49 -0400

    latest change for cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,84 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+        
+       # browser()
+        m <- NULL
+       
+        #pass in a new matrix into the function instead of having to call it all over again
+        set <- function(y) 
+        {
+                x <<- y
+                m <<- NULL
+        }
+        
+        #return whatever was passed into the function
+        get <- function() x
+        
+        
+        #uses the solve function on the original matrix to return the invesrse matrix
+        getinverse <- function () 
+        {
+                print ("in getinverse function")
+                            
+                
+                if(!is.null(m)) 
+                {
+                        message("getting cached matrix")
+                        #return(m)
+                }
+                else 
+                {
+                        print ("creatig the inverted matrix for the first time")
+                        m <<- solve(x)
+                }
+                
+               # print (m)
+                print ("done in getinverse")
+                
+                
+                #returns the inverse of the matrix x which was passed in
+                #m <<- solve(x)
+             return (m)
+        }
+        
+        #the list of methods to call on the function
+        list(set = set, get = get,
+             getinverse = getinverse)
+       
+#end of makeCacheMatrix        
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+
+     #  browser()
+        print ("in cachesolve, about to call getinv")
+       
+        
+        m <- x$getinverse()
+       print ("in cachesolve, done in getinv")
+        
+        
+    #   if(!is.null(m)) 
+    #    {
+    #       message("getting cached matrix")
+          # return(m)
+    #    }
+    #   else {m <- x$getinverse()}
+       
+        #the data returns the original matrix that was passed in
+        data <- x$get()
+       
+        
+        #returns the inverted matrix
+      return(m)
+        
+#end of cacheSolve
 }
+
